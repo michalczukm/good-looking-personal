5add65b030dcec3b52e19366e207f8a882441ed9
Michal Michalczuk <michalczukm@gmail.com>
2015-10-05 02:05:51 +0200

    remove unused gulp tasks

--- gulp/server.js
+++ gulp/server.js
@@ -42,22 +42,10 @@ function browserSyncInit(baseDir, browser) {
   });
 }
 
-browserSync.use(browserSyncSpa({
-  selector: '[ng-app]'// Only needed for angular apps
-}));
-
 gulp.task('serve', ['watch'], function () {
   browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
 });
 
 gulp.task('serve:dist', ['build'], function () {
   browserSyncInit(conf.paths.dist);
-});
-
-gulp.task('serve:e2e', ['inject'], function () {
-  browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
-});
-
-gulp.task('serve:e2e-dist', ['build'], function () {
-  browserSyncInit(conf.paths.dist, []);
 });
